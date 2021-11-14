b7575c2c7ba5639dc3746cdb4c402540ae2223f9
Dummyapt <duha.cinar@gmail.com>
2021-11-14 21:49:03 +0100

    Finished banking application

--- src/de/dummyapt/sandbox/bankingapplication/BankAccount.java
+++ src/de/dummyapt/sandbox/bankingapplication/BankAccount.java
@@ -1,13 +1,60 @@
 package de.dummyapt.sandbox.bankingapplication;
 
 class BankAccount {
-    double balance;
-    String firstName;
-    String lastName;
+    private final String id;
+    private final String name;
+    private int balance;
+    private int previousTransaction;
 
-    public BankAccount(double balance, String firstName, String lastName) {
-        this.balance = balance;
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public BankAccount(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void deposit(int amount) {
+        if (amount <= 0)
+            log("Error! Deposit can't be below or equal to 0.");
+        else {
+            balance += amount;
+            previousTransaction = amount;
+            log("Deposit succeeded!");
+            log(String.format("New balance: %s%n", balance));
+        }
+    }
+
+    void withdraw(int amount) {
+        if (amount <= 0)
+            log("Error! Withdraw can't be below or equal to 0.");
+        else {
+            balance -= amount;
+            previousTransaction = -amount;
+            log("Withdraw succeeded!");
+            log(String.format("New balance: %s", balance));
+        }
+    }
+
+    String getPreviousTransaction() {
+        if (previousTransaction > 0)
+            return String.format("Deposited: %s", previousTransaction);
+        else if (previousTransaction < 0)
+            return String.format("Withdrawn: %s", Math.abs(previousTransaction));
+        else
+            return "No previous transaction!";
+    }
+
+    private void log(String s) {
+        System.out.println(s);
     }
 }
--- src/de/dummyapt/sandbox/bankingapplication/BankingApplication.java
+++ src/de/dummyapt/sandbox/bankingapplication/BankingApplication.java
@@ -3,7 +3,55 @@ package de.dummyapt.sandbox.bankingapplication;
 import java.util.Scanner;
 
 class BankingApplication {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final BankAccount BANK_ACCOUNT = new BankAccount("1208", "Dummy");
+
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
+        log(String.format("Welcome, %s!", BANK_ACCOUNT.getName()));
+        log(String.format("Your ID is %s!%n", BANK_ACCOUNT.getId()));
+        showMenu();
+    }
+
+    private static void showMenu() {
+        log("""
+                ===============================
+                        Enter an option
+                ===============================
+                A - Check balance
+                B - Deposit
+                C - Withdraw
+                D - Previous transaction
+                E - Exit""");
+
+        char option = SCANNER.next().toUpperCase().charAt(0);
+        switch (option) {
+            case 'A' -> {
+                log(String.format("Balance: %s", BANK_ACCOUNT.getBalance()));
+                showMenu();
+            }
+            case 'B' -> {
+                log("Amount to deposit:");
+                BANK_ACCOUNT.deposit(Math.abs(SCANNER.nextInt()));
+                showMenu();
+            }
+            case 'C' -> {
+                log("Amount to withdraw:");
+                BANK_ACCOUNT.withdraw(Math.abs(SCANNER.nextInt()));
+                showMenu();
+            }
+            case 'D' -> {
+                log(String.format("Last transaction: %n%s", BANK_ACCOUNT.getPreviousTransaction()));
+                showMenu();
+            }
+            case 'E' -> log(String.format("Goodbye, %s", BANK_ACCOUNT.getName()));
+            default -> {
+                log("Error! Wrong option.");
+                showMenu();
+            }
+        }
+    }
+
+    private static void log(String s) {
+        System.out.println(s);
     }
 }
