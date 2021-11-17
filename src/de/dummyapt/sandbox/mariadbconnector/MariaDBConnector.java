89ea8fb7136d47cd856d55b92e73492e7534f577
Dummyapt <duha.cinar@gmail.com>
2021-11-17 23:06:53 +0100

    Added line break

--- src/de/dummyapt/sandbox/mariadbconnector/MariaDBConnector.java
+++ src/de/dummyapt/sandbox/mariadbconnector/MariaDBConnector.java
@@ -35,6 +35,6 @@ class MariaDBConnector {
                 -------------------------------
                         Table structure
                 -------------------------------
-                %s""", table);
+                %s%n""", table);
     }
 }
